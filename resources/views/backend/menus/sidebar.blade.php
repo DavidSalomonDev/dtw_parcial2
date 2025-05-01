55f294f6abc7162370e588765b144c8da14296ff
davidSalomon <mv12013@ues.edu.sv>
2025-05-01 11:42:31 -0600

    formato sidebar

--- resources/views/backend/menus/sidebar.blade.php
+++ resources/views/backend/menus/sidebar.blade.php
@@ -1,7 +1,6 @@
-
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <a href="#" class="brand-link">
-        <img src="{{ asset('images/logo.png') }}" alt="Logo" class="brand-image img-circle elevation-3" >
+        <img src="{{ asset('images/logo.png') }}" alt="Logo" class="brand-image img-circle elevation-3">
         <span class="brand-text font-weight" style="color: white">PANEL DE CONTROL</span>
     </a>
 
@@ -12,45 +11,45 @@
 
                 <!-- ROLES Y PERMISO -->
                 @can('sidebar.roles.y.permisos')
-                 <li class="nav-item">
+                    <li class="nav-item">
 
-                     <a href="#" class="nav-link nav-">
-                        <i class="far fa-edit"></i>
-                        <p>
-                            Roles y Permisos
-                            <i class="fas fa-angle-left right"></i>
-                        </p>
-                    </a>
+                        <a href="#" class="nav-link nav-">
+                            <i class="far fa-edit"></i>
+                            <p>
+                                Roles y Permisos
+                                <i class="fas fa-angle-left right"></i>
+                            </p>
+                        </a>
 
-                    <ul class="nav nav-treeview">
-                        <li class="nav-item">
-                            <a href="{{ route('admin.roles.index') }}" target="frameprincipal" class="nav-link">
-                                <i class="far fa-circle nav-icon"></i>
-                                <p>Rol y Permisos</p>
-                            </a>
-                        </li>
-<li class="nav-item">
-    <a href="{{ route('admin.roles.index') }}" target="frameprincipal" class="nav-link">
-        <i class="far fa-circle nav-icon"></i>
-        <p>Rol y Permisos</p>
-    </a>
-</li>
+                        <ul class="nav nav-treeview">
+                            <li class="nav-item">
+                                <a href="{{ route('admin.roles.index') }}" target="frameprincipal" class="nav-link">
+                                    <i class="far fa-circle nav-icon"></i>
+                                    <p>Rol y Permisos</p>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="{{ route('admin.roles.index') }}" target="frameprincipal" class="nav-link">
+                                    <i class="far fa-circle nav-icon"></i>
+                                    <p>Rol y Permisos</p>
+                                </a>
+                            </li>
 
-                        <li class="nav-item">
-                            <a href="{{ route('admin.permisos.index') }}" target="frameprincipal" class="nav-link">
-                                <i class="far fa-circle nav-icon"></i>
-                                <p>Usuario</p>
-                            </a>
-                        </li>
+                            <li class="nav-item">
+                                <a href="{{ route('admin.permisos.index') }}" target="frameprincipal" class="nav-link">
+                                    <i class="far fa-circle nav-icon"></i>
+                                    <p>Usuario</p>
+                                </a>
+                            </li>
 
-                    </ul>
-                 </li>
-                 <li class="nav-item">
-                    <a href="{{ url('/xml-to-json') }}" target="frameprincipal" class="nav-link">
-                        <i class="fas fa-book nav-icon"></i>
-                        <p>Ver Libros</p>
-                    </a>
-                </li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a href="{{ url('/xml-to-json') }}" target="frameprincipal" class="nav-link">
+                            <i class="fas fa-book nav-icon"></i>
+                            <p>Ver Libros</p>
+                        </a>
+                    </li>
                 @endcan
                 <li class="sidebar-item">
                     <a class="sidebar-link" href="{{ route('libros.index') }}">
@@ -65,9 +64,3 @@
 
     </div>
 </aside>
-
-
-
-
-
-
